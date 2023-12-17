2f3ff9f2dc10e96d22e42caf77cdb14d90199c98
Salim Ali <u14262208@tuks.co.za>
2023-12-17 12:42:21 +0200

    added constants

---
+++ lib/utility/constants.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+
+const Color grey = Color(0xFFF2F3F5);
+const Color dark_grey = Color(0xFF9E9E9E);
+const Color black = Color(0xFF181818);
+const Color red = Color(0xFFFF0626);
+const Color green = Color(0xFF019C00);
+const Color orange = Color(0xFFFFCA51);
+
+const List<Map<String, String>> categories = [{ "icon": "sandwich", "category": "Mexican" },
+                                              { "icon": "sushi", "category": "Asian" },
+                                              { "icon": "fries", "category": "Fast Food" },
+                                              { "icon": "burger", "category": "American" },
+                                              { "icon": "salad", "category": "Vegetarian" },
+                                              { "icon": "pizza", "category": "Italian" }];
